e929a3b2118d1dd3b503e100d95bed260d595553
JoseCastro16 <jose.castro79@myhunter.cuny.edu>
2022-10-03 22:09:27 -0400

    center icon

--- screens/Profile.js
+++ screens/Profile.js
@@ -10,60 +10,72 @@
 
 // export default Profile;
 
-import React, { useState, useEffect } from 'react';
-import { Image, View, Platform, TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState, useEffect } from "react";
+import {
+  Image,
+  View,
+  Platform,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 export default function UploadImage() {
- const [image, setImage] = useState(null);
- const addImage=async()=>{
-   let _image = await ImagePicker.launchImageLibraryAsync({
-       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-       allowsEditing: true,
-       aspect: [4,3],
-       quality: 1,
-     });
-     console.log(JSON.stringify(_image));
-     if (!_image.cancelled) {
-       setImage(_image.uri);
-     }
- };
- return (
-           <View style={imageUploaderStyles.container}>
-               {
-                   image  && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-               }
-                   <View style={imageUploaderStyles.uploadBtnContainer}>
-                       <TouchableOpacity onPress={addImage} style={imageUploaderStyles.uploadBtn} >
-                           <Text>{image ? 'Edit' : 'Upload'} Image</Text>
-                           <AntDesign name="camera" size={20} color="black" />
-                       </TouchableOpacity>
-                   </View>
-           </View>
- );
+  const [image, setImage] = useState(null);
+  const addImage = async () => {
+    let _image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(JSON.stringify(_image));
+    if (!_image.cancelled) {
+      setImage(_image.uri);
+    }
+  };
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={imageUploaderStyles.container}>
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+        <View style={imageUploaderStyles.uploadBtnContainer}>
+          <TouchableOpacity
+            onPress={addImage}
+            style={imageUploaderStyles.uploadBtn}
+          >
+            <Text>{image ? "Edit" : "Upload"} Image</Text>
+            <AntDesign name="camera" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
-const imageUploaderStyles=StyleSheet.create({
-   container:{
-       elevation:2,
-       height:200,
-       width:200,
-       backgroundColor:'#efefef',
-       position:'relative',
-       borderRadius:999,
-       overflow:'hidden',
-   },
-   uploadBtnContainer:{
-       opacity:0.7,
-       position:'absolute',
-     
-       bottom:0,
-       backgroundColor:'lightgrey',
-       width:'100%',
-       height:'25%',
-   },
-   uploadBtn:{
-       display:'flex',
-       alignItems:"center",
-       justifyContent:'center'
-   }
-})
+const imageUploaderStyles = StyleSheet.create({
+  container: {
+    elevation: 2,
+    height: 200,
+    width: 200,
+    backgroundColor: "#efefef",
+    position: "relative",
+    borderRadius: 999,
+    overflow: "hidden",
+  },
+  uploadBtnContainer: {
+    opacity: 0.7,
+    position: "absolute",
+
+    bottom: 0,
+    backgroundColor: "lightgrey",
+    width: "100%",
+    height: "25%",
+  },
+  uploadBtn: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
