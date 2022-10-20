8e81ab18889bc4730e45ffb47bcc03fc53c7b32b
Matthew Roberts <slashgray147@gmail.com>
2022-10-19 22:16:29 -0400

    um

--- api/client.js
+++ api/client.js
@@ -1,5 +1,4 @@
 import axios from "axios";
 
-// export default axios.create({ baseURL: "http://192.168.12.119:8000" })
 // export default axios.create({ baseURL: "http://IP_ADDRESS_OF_WIFI:8000" })
 export default axios.create({ baseURL: "http://localhost:8000" })
